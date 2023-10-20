158d92073fcf85de1e0d6c3433dd52a94141cd58
HanslettTheDev <HanslettTheDev@gmail.com>
2023-10-20 23:05:40 +0100

    feat: refactored the config file and settings and updated the main code base

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 # from configparser import ConfigParser
 
 import subprocess
+import os
+from configparser import ConfigParser
 from pyvda import AppView, get_apps_by_z_order, VirtualDesktop, get_virtual_desktops
 
 class MakeMaximus:
     def __init__(self, config) -> None:
-        self.window_names = ["Main Desktop", "Ramims", "Game Dev", "NWBS Updates"]
+        self.config = config
+        self.config_parser = ConfigParser()
+        self.config_parser.read(self.config)
         
 
-        self.window_settings = {
-            "Main Desktop": {
-                "apps": "Any"
-                },
-            "Ramims": {
-                "apps": ["code", "chrome", "cmd"],
-                "folder": r"/MummyRach",
-                "window_sizes": {
-                    "code": (1000, 500),
-                    "chrome": (1000, 500),
-                    "cmd": (700, 300)
-                }
-                }
-        }
+        # set default settings
+        setattr(self, "default_settings", self.config_parser["DefaultSettings"])
 
+        self.desktops_only = [desktop for desktop in self.config_parser.sections() if desktop.startswith("Desktop.")]
+
+        for desktop_name in self.desktops_only:
+            setattr(
+                self, 
+                desktop_name.split(".")[-1], 
+                self.config_parser["Desktop." + desktop_name.split(".")[-1]]
+            )
+        
     def create_virtual_desktops(self):
         active_desktops = len(get_virtual_desktops())
-
-        # if active_desktops > 1:
+        
+        print(len(get_virtual_desktops()))
+        for desktop in get_virtual_desktops():
+            try:
+                if getattr(self, desktop.name):
+                    self.desktops_only.pop(self.desktops_only.index("Desktop." + desktop.name))
+                
+            except AttributeError:
+                continue
+        print(len(get_virtual_desktops()))
+        #if active_desktops > 1:
         #     return self.recreate_environments()
-
-        for window_name in self.window_names:
+        
+        for window_name in self.desktops_only:
             virtual_desktop = VirtualDesktop.create()
-            virtual_desktop.rename(window_name)
+            virtual_desktop.rename(self.config_parser[window_name]["desktop_name"])
 
     
     def place_apps(self):
@@ -42,21 +52,16 @@ class MakeMaximus:
         for desktop in active_desktops:
             # print(desktop.name)
             try:
-                if self.window_settings[desktop.name]:
+                if getattr(self, desktop):
                     target_desktop = VirtualDesktop(desktop.number)
                     target_desktop.go()
-                    subprocess.Popen(["code"])
-                    self.window_settings[desktop.name]
+                    os.system("start cmd.exe")
             except KeyError:
                 continue
 
 
 
 
-maximus = MakeMaximus("hello")
-# maximus.create_virtual_desktops()
-maximus.place_apps()
-
-# theconfig = ConfigParser()
-# print(theconfig.read("maximus_config.ini")) 
-# print(theconfig.sections())
+maximus = MakeMaximus("maximus_config.ini")
+maximus.create_virtual_desktops()
+#maximus.place_apps()
